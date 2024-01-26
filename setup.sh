31071eccbeb9433fb0a4d343c3198919b5947563
gdiasbruno <gdias.bruno@gmail.com>
2024-01-26 09:19:48 +0000

    Add setup script on runpod

---
+++ setup.sh
@@ -0,0 +1,13 @@
+pip install -r requirements_2.txt
+pip install gdown
+# dataset
+gdown https://drive.google.com/uc?id=1FAfy-2X2Ly_HiimafwTLtpiRVgOlX7yV
+# weights
+gdown https://drive.google.com/uc?id=101aViiaJWXQ-dWUwK6-FPSjOxsl_wCnm
+
+apt-get update && apt-get install unzip
+
+unzip all_pics.zip
+
+git config --global user.name "gdiasbruno"
+git config --global user.email gdias.bruno@gmail.com
